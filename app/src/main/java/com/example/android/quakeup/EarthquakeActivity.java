974d27e2ea193a9edafbb526fe894bd521135a63
Shovan Shah <realshovanshah@gmail.com>
2020-07-04 17:36:21 +0545

    Threading with AsyncTasks

--- app/src/main/java/com/example/android/quakeup/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakeup/EarthquakeActivity.java
@@ -19,66 +19,92 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
+    private EarthquakeAdapter mEarthquakeAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-//        ArrayList<EarthquakeModel> earthquakes = new ArrayList<>();
-//        earthquakes.add(new EarthquakeModel("6.8", "India", "2 Feb, 2016"));
-//        earthquakes.add(new EarthquakeModel("5.1", "China", "20 July, 2017"));
-//        earthquakes.add(new EarthquakeModel("5.8", "Bangladesh", "13 Oct, 2017"));
-//        earthquakes.add(new EarthquakeModel("7.2", "Pakistan", "28 Dec, 2016"));
-//        earthquakes.add(new EarthquakeModel("6.3", "Nepal", "3 Mar, 2018"));
-
         // Find a reference to the {@link ListView} in the layout
         final ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, Utils.extractEarthquakes());
+        mEarthquakeAdapter = new EarthquakeAdapter(this, new ArrayList<EarthquakeModel>());
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                EarthquakeModel currentEarthquake = adapter.getItem(i);
 
-                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+        if (earthquakeListView != null) {
+            earthquakeListView.setAdapter(mEarthquakeAdapter);
+            earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    EarthquakeModel currentEarthquake = mEarthquakeAdapter.getItem(i);
 
-                PackageManager packageManager = getPackageManager();
-                List<ResolveInfo> activities = packageManager.queryIntentActivities(browserIntent, PackageManager.MATCH_DEFAULT_ONLY);
-                boolean isIntentSafe = activities.size() > 0;
+                    Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
 
-                if (isIntentSafe) {
-                    startActivity(browserIntent);
+                    PackageManager packageManager = getPackageManager();
+                    List<ResolveInfo> activities = packageManager.queryIntentActivities(browserIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                    boolean isIntentSafe = activities.size() > 0;
+
+                    if (isIntentSafe) {
+                        startActivity(browserIntent);
+                    }
+
+                    //                Create a chooser
+                    //                Intent chooser = Intent.createChooser(browserIntent, "Select Browser");
+                    //                if (browserIntent.resolveActivity(getPackageManager()) != null) {
+                    //                    startActivity(chooser);
+                    //                }
                 }
+            });
+        }
 
-//                Create a chooser
-//                Intent chooser = Intent.createChooser(browserIntent, "Select Browser");
-//                if (browserIntent.resolveActivity(getPackageManager()) != null) {
-//                    startActivity(chooser);
-//                }
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<EarthquakeModel>> {
+        @Override
+        protected List<EarthquakeModel> doInBackground(String... urls) {
+            String jsonResponse = "";
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
             }
-        });
+            List<EarthquakeModel> result = Utils.fetchEarthquakeData(urls[0]);
 
-        Log.d(LOG_TAG, "onCreate: " + Utils.extractEarthquakes());
+            return result;
+        }
+
+        ;
+
+        @Override
+        protected void onPostExecute(List<EarthquakeModel> earthquakeData) {
+            mEarthquakeAdapter.clear();
+
+            if (earthquakeData != null && !earthquakeData.isEmpty()) {
+                mEarthquakeAdapter.addAll(earthquakeData);
+            }
+        }
     }
 
 }
